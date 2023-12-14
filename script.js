b70d6d5c5205e21caffa489564bdbbfd79809d07
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-14 13:58:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	function firstNonRepeatedChar(str) {
+  const charCountMap = new Map();
+
+  for (const char of str) {
+    charCountMap.set(char, (charCountMap.get(char) || 0) + 1);
+  }
+
+  for (const char of str) {
+    if (charCountMap.get(char) === 1) {  
+      return char; 
+    }
+  }
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
